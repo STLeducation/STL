2e17d4c3ee98e030d981cd4bc49d9689e3a3e149
STLeducation <stlprojetostl@gmail.com>
2025-07-26 23:08:12 -0300

    Update frontend_utils.py
    Updated docstrings of both functions

--- STL 2025/frontend_utils.py
+++ STL 2025/frontend_utils.py
@@ -2,6 +2,15 @@ import os
 from pygrabber.dshow_graph import FilterGraph
 
 def find_video(user_query = str):
+    """
+    Checks if search has a corresponding file to be displayed
+
+    Args:
+        user_query (str): search input
+    
+    Returns:
+        word path if it has a result, None if no result is found
+    """
     user_query = user_query.lower()
     word_path = os.path.join(".\palavras",f"{user_query}.mp4")
     if os.path.exists(word_path):
@@ -10,6 +19,12 @@ def find_video(user_query = str):
         return None
 
 def get_available_cameras() :
+    """
+    Gets all cameras in device
+
+    Returns:
+        Dictionary of camera name and coresponding index
+    """
     devices = FilterGraph().get_input_devices()
 
     available_cameras = {}
